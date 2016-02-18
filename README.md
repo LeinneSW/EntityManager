d17a2b313373efdcd59f7552b1a06af8b959a19f
승원 <milk5590@naver.com>
2016-02-19 03:08:51 +0900

    First commit small change

---
+++ README.md
@@ -0,0 +1,104 @@
+# EntityManager
+  
+Author(제작자): **[@milk0417(승원)](https://github.com/milk0417)**  
+  
+자매품(Nukkit): [EntityManager-Nukkit](https://github.com/SW-Team/EntityManager)
+    
+**[NOTICE] This plug-in is not perfect, the entity may move abnormally (PHP7 Only)**
+  
+EntityManager is a plugin for managing entities, literally.  
+Most entities are moving around, and jumps if needed.  
+  
+EntityManager also has simple API for developers,  
+such as **clear()** or **create()**.  
+  
+See documentation page for details.  
+  
+**[알림] 이 플러그인은 완벽하지 않으며 엔티티가 비정상적으로 움직일 수 있습니다 (PHP7만 지원합니다)**  
+  
+엔티티매니저는 말 그대로 엔티티를 관리하는 플러그인을 의미합니다.  
+많은 엔티티들은 주위를 돌아다니거나 뛰어다닙니다.  
+
+엔티티매니저는 또한 개발자 여러분을 위해  
+**clear()** 또는 **create()** 와 같은 간단한 API가 제공됩니다.  
+  
+자세한 사항은 아래를 보시기 바랍니다
+
+### YAML data
+  * config.yml
+``` yml
+entity:
+  explode: false #Whether the entity explosion
+spawn:
+  rand: "1/4" #Entity spawn probability
+  tick: 100 #Entity spawn period
+autospawn:
+  turn-on: true #Whether auto-spawn
+  radius: 25 #Radius will spawn location
+autoclear:
+  turn-on: true #Whether the entity automatically removed
+  tick: 6000 #Entity remove period
+  entities: [Projectile, DroppedItem] #List of entities to be removed
+```
+  * spawner.yml
+    * TODO
+  * drops.yml
+    * TODO
+  
+### Commands(명령어)
+  * /entitymanager
+    * usage: /entitymanager (check|remove|spawn)
+    * permission: entitymanager.command
+  * /entitymanager check
+    * usage: /entitymanager check (Level="")
+    * permission: entitymanager.command.check
+    * description: Check the number of entities(If blank, it is set as a default Level)
+  * /entitymanager remove
+    * usage: /entitymanager remove (Level="")
+    * permission: entitymanager.command.remove
+    * description: Remove all entities in Level(If blank, it is set as a default Level)
+  * /entitymanager spawn:
+    * usage: /entitymanager spawn (type) (x="") (y="") (z="") (Level="")
+    * permission: entitymanager.command.spawn
+    * description: literally(If blank, it is set as a Player)
+
+### Method(메소드)
+  * EntityManager
+    * public static function clear(array $type = [BaseEntity::class], Level $level = null) : void
+    * public static function create(int|string $type, Position $pos, ...$args) : Entity
+  * BaseEntity
+    * public function isMovement() : bool
+    * public function isFriendly() : bool
+    * public function isWallCheck() : bool
+    * public function setMovement(bool $value) : void
+    * public function setFriendly(bool $value) : void
+    * public function setWallCheck(bool $value) : void
+  * Monster
+    * public function getDamage(int $difficulty = null) : float
+    * public function setDamage(float|float[] $damage, int $difficulty = null) : void
+  * PigZombie
+    * public function isAngry() : bool
+    * public function setAngry(int $angry) : void
+
+### Method Examples(메소드 예시)
+``` php
+//Entity Method
+foreach(EntityManager::getEntity() as $entity){
+    if(!$entity->isMovement()){
+        $entity->setMovement(true);
+    }
+    if($entity instanceof Monster){
+        $entity->setDamage(10);
+
+        $entity->setMaxDamage(10);
+        $entity->setMinDamage(10);
+    }
+}
+
+//Create Entity
+$arrow = EntityManager::create("Arrow", $pos, $player, true);
+$zombie = EntityManager::create("Zombie", $pos);
+
+//Remove Entity
+EntityManager::clear([BaseEntity::class, Projectile::class, Item::class]);
+```
---
+++ src/milk/entitymanager/EntityManager.php
@@ -0,0 +1,308 @@
+<?php
+
+namespace milk\entitymanager;
+
+use milk\entitymanager\task\AutoClearTask;
+use milk\entitymanager\task\AutoSpawnTask;
+use milk\pureentities\entity\animal\walking\Chicken;
+use milk\pureentities\entity\animal\walking\Cow;
+use milk\pureentities\entity\animal\walking\Mooshroom;
+use milk\pureentities\entity\animal\walking\Ocelot;
+use milk\pureentities\entity\animal\walking\Pig;
+use milk\pureentities\entity\animal\walking\Rabbit;
+use milk\pureentities\entity\animal\walking\Sheep;
+use milk\pureentities\entity\BaseEntity;
+use milk\pureentities\entity\monster\flying\Blaze;
+use milk\pureentities\entity\monster\flying\Ghast;
+use milk\pureentities\entity\monster\walking\CaveSpider;
+use milk\pureentities\entity\monster\walking\Creeper;
+use milk\pureentities\entity\monster\walking\Enderman;
+use milk\pureentities\entity\monster\walking\IronGolem;
+use milk\pureentities\entity\monster\walking\PigZombie;
+use milk\pureentities\entity\monster\walking\Silverfish;
+use milk\pureentities\entity\monster\walking\Skeleton;
+use milk\pureentities\entity\monster\walking\SnowGolem;
+use milk\pureentities\entity\monster\walking\Spider;
+use milk\pureentities\entity\monster\walking\Wolf;
+use milk\pureentities\entity\monster\walking\Zombie;
+use milk\pureentities\entity\monster\walking\ZombieVillager;
+use milk\pureentities\entity\projectile\FireBall;
+use milk\pureentities\PureEntities;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\entity\Entity;
+use pocketmine\entity\Human;
+use pocketmine\entity\Living;
+use pocketmine\entity\Projectile;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\entity\ExplosionPrimeEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\entity\Item as ItemEntity;
+
+class EntityManager extends PluginBase implements Listener{
+
+    public static $data;
+    public static $drops;
+    public static $spawner;
+
+    public function __construct(){
+        $classes = [
+            Blaze::class,
+            CaveSpider::class,
+            Chicken::class,
+            Cow::class,
+            Creeper::class,
+            Enderman::class,
+            Ghast::class,
+            IronGolem::class,
+            //MagmaCube::class,
+            Mooshroom::class,
+            Ocelot::class,
+            Pig::class,
+            PigZombie::class,
+            Rabbit::class,
+            Sheep::class,
+            Silverfish::class,
+            Skeleton::class,
+            //Slime::class,
+            SnowGolem::class,
+            Spider::class,
+            Wolf::class,
+            Zombie::class,
+            ZombieVillager::class,
+            FireBall::class
+        ];
+        foreach($classes as $name){
+            Entity::registerEntity($name);
+            if(
+                $name == IronGolem::class
+                || $name == FireBall::class
+                || $name == SnowGolem::class
+                || $name == ZombieVillager::class
+            ){
+                continue;
+            }
+            $item = Item::get(Item::SPAWN_EGG, $name::NETWORK_ID);
+            if(!Item::isCreativeItem($item)){
+                Item::addCreativeItem($item);
+            }
+        }
+    }
+
+    public function onEnable(){
+        @mkdir($this->getDataFolder());
+        $this->saveResource("config.yml", false);
+        if($this->getConfig()->exists("spawn")){
+            $this->saveResource("config.yml", true);
+            $this->reloadConfig();
+            $this->getServer()->getLogger()->info(TextFormat::GOLD . "[EntityManager]Your config has been updated. Please check \"config.yml\" file and restart the server.");
+        }elseif($this->getConfig()->exists("spawner")){
+            $this->getConfig()->remove("spawner");
+            $this->getConfig()->save();
+        }
+
+        self::$data = $this->getConfig()->getAll();
+        self::$drops = (new Config($this->getDataFolder() . "drops.yml", Config::YAML))->getAll();
+
+        if(isset(self::$data["entity"]["explode"])){
+            self::$data["entity"]["explodeMode"] = "none";
+            unset(self::$data["entity"]["explode"]);
+        }
+
+        /*self::$drops = [
+            "Zombie" => [
+                #[Item id, Item meta, Count]
+                [Item::FEATHER, 0, "1,10"]
+            ],
+            "Creeper" => [
+                [Item::FEATHER, 0, "1,10"]
+            ],
+        ];*/
+
+        if($this->getData("autospawn.turn-on", true)){
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new AutoSpawnTask($this), $this->getData("autospawn.tick", 100));
+        }
+        if($this->getData("autoclear.turn-on", true)){
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new AutoClearTask($this), $this->getData("autoclear.tick", $this->getData("autoclear.tick", 6000)));
+        }
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info(TextFormat::GOLD . "[EntityManager]Plugin has been enabled");
+    }
+
+    public function onDisable(){
+        $this->getServer()->getLogger()->info(TextFormat::GOLD . "[EntityManager]Plugin has been disable");
+    }
+
+    public static function clear(array $type = [BaseEntity::class], Level $level = null){
+        $level = $level === null ? Server::getInstance()->getDefaultLevel() : $level;
+        foreach($level->getEntities() as $id => $ent){
+            foreach($type as $t){
+                if(is_a($ent, $t, true)){
+                    $ent->close();
+                    continue;
+                }
+            }
+        }
+    }
+
+    public function getData(string $key, $defaultValue){
+        $vars = explode(".", $key);
+        $base = array_shift($vars);
+        if(!isset(self::$data[$base])){
+            return $defaultValue;
+        }
+
+        $base = self::$data[$base];
+        while(count($vars) > 0){
+            $baseKey = array_shift($vars);
+            if(!is_array($base) or !isset($base[$baseKey])){
+                return $defaultValue;
+            }
+            $base = $base[$baseKey];
+        }
+        return $base;
+    }
+
+    public function ExplosionPrimeEvent(ExplosionPrimeEvent $ev){
+        switch($this->getData("entity.explodeMode", "none")){
+            case "onlyEntity":
+                $ev->setBlockBreaking(false);
+                break;
+            case "none":
+                $ev->setForce(0);
+                $ev->setBlockBreaking(false);
+                break;
+        }
+    }
+
+    public function EntityDeathEvent(EntityDeathEvent $ev){
+        $reflect = new \ReflectionClass(get_class($ev->getEntity()));
+        if(!isset(self::$drops[$reflect->getShortName()])){
+            return;
+        }
+
+        $drops = [];
+        foreach(self::$drops[$reflect->getShortName()] as $key => $data){
+            if(!isset($data[0]) || !isset($data[1]) || !isset($data[2])){
+                unset(self::$drops[$reflect->getShortName()][$key]);
+                continue;
+            }
+
+            $count = explode(",", $data[2]);
+            $item = Item::get($data[0], $data[1]);
+            $item->setCount(max(mt_rand(...$count), 0));
+            $drops[] = $item;
+        }
+        $ev->setDrops($drops);
+    }
+
+    public function onCommand(CommandSender $i, Command $cmd, $label, array $sub){
+        $output = "[EntityManager]";
+        switch(array_shift($sub)){
+            case "remove":
+                if(!$i->hasPermission("entitymanager.command.remove")){
+                    $i->sendMessage(TextFormat::RED . "You do not have permission to use this command");
+                    return true;
+                }
+
+                if(isset($sub[0])){
+                    $level = $this->getServer()->getLevelByName($sub[0]);
+                }else{
+                    $level = $i instanceof Player ? $i->getLevel() : null;
+                }
+
+                self::clear([BaseEntity::class, Projectile::class, ItemEntity::class], $level);
+                $output .= "All spawned entities were removed";
+                break;
+            case "check":
+                if(!$i->hasPermission("entitymanager.command.check")){
+                    $i->sendMessage(TextFormat::RED . "You do not have permission to use this command");
+                    return true;
+                }
+
+                $human = 0;
+                $living = 0;
+                $item = 0;
+                $projectile = 0;
+                $other = 0;
+                if(isset($sub[0])){
+                    $level = $this->getServer()->getLevelByName($sub[0]);
+                }else{
+                    $level = $i instanceof Player ? $i->getLevel() : $this->getServer()->getDefaultLevel();
+                }
+
+                foreach($level->getEntities() as $id => $ent) {
+                    if($ent instanceof Human){
+                        $human++;
+                    }elseif($ent instanceof Living){
+                        $living++;
+                    }elseif($ent instanceof ItemEntity){
+                        $item++;
+                    }elseif($ent instanceof Projectile){
+                        $projectile++;
+                    }else{
+                        $other++;
+                    }
+                }
+
+                $output = "--- All entities in Level \"{$level->getName()}\" ---\n";
+                $output .= TextFormat::YELLOW . "Human: $human\n";
+                $output .= TextFormat::YELLOW . "Living: $living\n";
+                $output .= TextFormat::YELLOW . "Items: $item\n";
+                $output .= TextFormat::YELLOW . "Projectiles: $projectile\n";
+                $output .= TextFormat::YELLOW . "Others: $other\n";
+                break;
+            case "create":
+                if(!$i->hasPermission("entitymanager.command.create")){
+                    $i->sendMessage(TextFormat::RED . "You do not have permission to use this command");
+                    return true;
+                }
+
+                if(!isset($sub[0]) or (!is_numeric($sub[0]) and gettype($sub[0]) !== "string")){
+                    $output .= "Entity's name is incorrect";
+                    break;
+                }
+
+                $pos = null;
+                if(count($sub) >= 4){
+                    $level = $this->getServer()->getDefaultLevel();
+                    if(isset($sub[4]) && ($k = $this->getServer()->getLevelByName($sub[4]))){
+                        $level = $k;
+                    }elseif($i instanceof Player){
+                        $level = $i->getLevel();
+                    }
+                    $pos = new Position($sub[1], $sub[2], $sub[3], $level);
+                }elseif($i instanceof Player){
+                    $pos = $i->getPosition();
+                }
+
+                if($pos == null){
+                    $output .= "usage: /$label create <id/name> (x) (y) (z) (level)";
+                    break;
+                }
+
+                $entity = PureEntities::create($sub[0], $pos);
+                if($entity == null){
+                    $output .= "An error occurred while summoning entity";
+                    break;
+                }
+                $entity->spawnToAll();
+                break;
+            default:
+                $output .= "usage: /$label <remove/check/create>";
+                break;
+        }
+        $i->sendMessage($output);
+        return true;
+    }
+
+}
